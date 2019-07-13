55269428811b12547587a62835af4584f663f349
HMassey <52863961+HMassey@users.noreply.github.com>
2019-07-13 19:45:52 +0100

    Added class of "row" to all main divs.

--- index.php
+++ index.php
@@ -4,36 +4,35 @@
     </head>
 
     <body>
-      <img src="assets/img/placeholderimg.jpg" width="50%" height="50%"></img>
+        <img src="assets/img/placeholderimg.jpg" width="50%" height="50%"></img>
+        <div id="top-hook" class="row">
+            <h1>We are a company that money</h1>
+            <p>We make the money.</p>
+        </div>
 
-      <div id="top-hook">
-        <h1>We are a company that money</h1>
-          <p>We make the money.</p>
-      </div>
-
-      <div id="block1">
+      <div id="block1" class="row">
         <h2>We value the thing</h2>
-          <p>Because that's what our customers love.</p>
+            <p>Because that's what our customers love.</p>
       </div>
 
-      <div id="block2">
+      <div id="block2" class="row">
         <h2>We do the thing right</h2>
-          <p>Because we check things like:</p>
-          <ul>
-            <li>Water pressure</li>
-            <li>House prices</li>
-            <li>Car alarms</li>
-            <li>Halal meat</li>
+            <p>Because we check things like:</p>
+            <ul>
+                <li>Water pressure</li>
+                <li>House prices</li>
+                <li>Car alarms</li>
+                <li>Halal meat</li>
           </ul>
       </div>
 
-      <div id="block3">
+      <div id="block3" class="row">
         <h2>In a variety of different styles</h2>
           <p>Handmade using real ink</p>
           <!-- Maybe slideshow of pictures?       -->
       </div>
 
-      <div id="block4">
+      <div id="block4" class="row">
         <h2>But don't just take our word for it</h2>
           <p>Here's something they said.</p>
             <div class="testimonials">
