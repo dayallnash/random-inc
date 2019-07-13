1b324fa29d7bceff70de9af7fbc306850975359f
Dale Nash <dalenashdn@gmail.com>
2019-07-13 22:51:50 +0100

    Randomly generate company name
    Fix some whitespace in index.php
    Change head.php to not use slim jquery
    include shared.js and shared.css in head.php
    Create index.js
    Create api\words.php

---
+++ js/index.js
@@ -0,0 +1,39 @@
+function generateCompanyName()
+{
+    $.ajax({
+        url: 'api/words.php?companyName',
+        success: writeCompanyName
+    });
+}
+
+function writeCompanyName(data)
+{
+    var companyNameH1 = $('#top-hook > h1');
+
+    companyNameH1.text('');
+
+    for (var i = 0; i < data.words.length; i++)
+    {
+        let word = data.words[i];
+
+        if (i !== 2) {
+            let wordArr = word.toLowerCase().split('');
+
+            for (var x = 0; x < wordArr.length; x++) {
+                if (x === 0) {
+                    word = wordArr[x].toUpperCase();
+                } else {
+                    word += wordArr[x];
+                }
+            }
+        }
+
+        let text = companyNameH1.text();
+
+        if (companyNameH1.text() === '') {
+            companyNameH1.text(word);
+        } else {
+            companyNameH1.text(text + ' ' + word);
+        }
+    }
+}
