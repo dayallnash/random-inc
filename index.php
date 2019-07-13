ce4ac29f8f3780391e29193d63cafadab0fcbea7
HMassey <52863961+HMassey@users.noreply.github.com>
2019-07-13 19:50:08 +0100

    Regression

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 
     <body>
         <img src="assets/img/placeholderimg.jpg" width="50%" height="50%"></img>
-        <div id="top-hook" class="row">
+        <div id="top-hook">
             <h1>We are a company that money</h1>
             <p>We make the money.</p>
         </div>
 
-      <div id="block1" class="row">
+      <div id="block1">
         <h2>We value the thing</h2>
             <p>Because that's what our customers love.</p>
       </div>
 
-      <div id="block2" class="row">
+      <div id="block2">
         <h2>We do the thing right</h2>
             <p>Because we check things like:</p>
             <ul>
@@ -26,13 +26,13 @@
           </ul>
       </div>
 
-      <div id="block3" class="row">
+      <div id="block3">
         <h2>In a variety of different styles</h2>
           <p>Handmade using real ink</p>
           <!-- Maybe slideshow of pictures?       -->
       </div>
 
-      <div id="block4" class="row">
+      <div id="block4">
         <h2>But don't just take our word for it</h2>
           <p>Here's something they said.</p>
             <div class="testimonials">
