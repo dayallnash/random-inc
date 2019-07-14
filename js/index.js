c027dd79194d5a371cf91d37f2431f0fa33a89b5
HMassey <52863961+HMassey@users.noreply.github.com>
2019-07-14 04:40:12 +0100

    I've added some comments to try and understand this block of code. My only confusion currently is where the word array is initialised. I understand it's utilisation fine.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 function generateCompanyName()
 {
     $.ajax({
-        url: 'api/words.php?companyName',
-        success: writeCompanyName
+        url: 'api/words.php?companyName', //url for API to ask for new words
+        success: writeCompanyName //execute the function if you get through
     });
 }
 
 function writeCompanyName(data)
 {
-    var companyNameH1 = $('#top-hook > h1');
+    var companyNameH1 = $('#top-hook > h1'); //make the heading a variable. This allows you to reinitialise
 
-    companyNameH1.text('');
+    companyNameH1.text(''); //returns the contents of the tag as a string? Not seen this method
 
     for (var i = 0; i < data.words.length; i++)
     {
-        let word = data.words[i];
+        let word = data.words[i]; //iterate the word array
 
         if (i !== 2) {
             let wordArr = word.toLowerCase().split('');
@@ -23,7 +23,7 @@ function writeCompanyName(data)
                 if (x === 0) {
                     word = wordArr[x].toUpperCase();
                 } else {
-                    word += wordArr[x];
+                    word += wordArr[x]; //this whole block capitalises the first letter of the word right?
                 }
             }
         }
