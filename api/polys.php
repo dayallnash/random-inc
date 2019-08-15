ca560c9c79b5e1240ec5d603e308e5add80d6331
HMassey <52863961+HMassey@users.noreply.github.com>
2019-08-15 02:34:36 +0100

    API Changes
    request a smaller object rather than perform maths on it.

--- api/polys.php
+++ api/polys.php
@@ -6,7 +6,7 @@ $count = rand(3, 10);
 
 if (isset($_GET['randomPolys'])) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://api.noopschallenge.com/polybot?count='.$count.'&minSides=4&maxSides=9');
+    curl_setopt($ch, CURLOPT_URL, 'https://api.noopschallenge.com/polybot?count='.$count.'&minSides=4&maxSides=9&width=100&height=100');
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
--- js/index.js
+++ js/index.js
@@ -68,17 +68,17 @@ function drawRandomPolys(data)
         canvas.width = "100";
         parent.appendChild(canvas); //This creates a canvas tag per shape dynamically.
         var context = canvas.getContext('2d');
-        var originX = Math.floor(data.polygons[i][0].x / 10);
-        var originY = Math.floor(data.polygons[i][0].y / 10);
+        var originX = Math.floor(data.polygons[i][0].x);
+        var originY = Math.floor(data.polygons[i][0].y);
 
         for (i2 = 0; i2 < data.polygons[i].length ; i2++)
             {
                 if (i2 === 0) {continue;}
 
-                var x = Math.floor(data.polygons[i][i2].x / 10);
-                var y = Math.floor(data.polygons[i][i2].y / 10);
-                var previousX = Math.floor(data.polygons[i][i2 - 1].x / 10);
-                var previousY = Math.floor(data.polygons[i][i2 - 1].y / 10);
+                var x = Math.floor(data.polygons[i][i2].x);
+                var y = Math.floor(data.polygons[i][i2].y);
+                var previousX = Math.floor(data.polygons[i][i2 - 1].x);
+                var previousY = Math.floor(data.polygons[i][i2 - 1].y);
 
                 context.moveTo(previousX, previousY);
                 context.lineTo(x, y);
